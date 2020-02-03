07a668b4696fba0df4671560bcf31f0d1a977ca0
Youngbin Kwon <39914288+youngkwon315@users.noreply.github.com>
2020-02-03 19:03:27 +0900

    2020 Changes
    1. 팀 점수 밸런스 기능 추가

--- player-input.js
+++ player-input.js
@@ -1,55 +1,68 @@
 var thisWeekPlayer = [];
+var Ascore = 0;
+var Bscore = 0;
 
 var messageBox = document.getElementById("output");
-var messageBox_2 = document.getElementById("output2");
 var messageBox2 = document.getElementById("outputTeamA");
 var messageBox3 = document.getElementById("outputTeamB");
 
 function insertFirstPlayers() {
-  var tdArr = [];
   var grid = document.getElementById("players");
   var checkBoxes = grid.getElementsByTagName("INPUT");
-  var playerNames = document.getElementById("players")
 
   for (var i = 0; i < checkBoxes.length; i++) {
     if(checkBoxes[i].checked==true) {
       var data = checkBoxes[i].parentNode.parentNode;
       var playerName = data.cells[1].innerHTML
-      var playerScore = data.cells[2].innerHTML
+      var playerScore = parseFloat(data.cells[2].innerHTML);
 
       thisWeekPlayer.push([playerName, playerScore]);
-      tdArr.push(playerName);
     }
   }
   messageBox.innerHTML = "{"+ thisWeekPlayer + "}" + "<br>";
-  //messageBox_2.innerHTML = tdArr + "<br>";
 }
 
-/*function insertPlayers() {
-  player.push({
-    선수명: name.value,
-    공격력: atk.value,
-  });
+function insertPlayers() {
+  var name = document.getElementById("name").value;
+  var score = parseFloat(document.getElementById("score").value);
+
+  thisWeekPlayer.push([
+    name,
+    score
+  ]);
   printPlayers();
-}*/
+}
 
-/*function printPlayers() {
-  name.value = "";
-  atk.value = "";
-
-  messageBox.innerHTML = JSON.stringify(player) + "<br>";
-}*/
+function printPlayers() {
+  messageBox.innerHTML = "{"+ thisWeekPlayer + "}" + "<br>";
+}
 
 function pickPlayers() {
+  var flag = true;
+  while (flag) {
+    flag = false;
+    selectMembers();
+    if ((Ascore - Bscore >= 2) || (Bscore - Ascore >= 2)) {
+      flag = true;
+    }
+  }
+}
+
+function selectMembers() {
   teamA = [];
+  var totalNum = thisWeekPlayer.length;
 
   for (var i = 0; i < thisWeekPlayer.length; i++) {
     var randomPlayer = thisWeekPlayer[Math.floor(Math.random() * thisWeekPlayer.length)];
     if (teamA.indexOf(randomPlayer) == -1) {
       teamA.push(randomPlayer);
+      Ascore += randomPlayer[1];
       thisWeekPlayer.splice(thisWeekPlayer.indexOf(randomPlayer), 1);
-      if (teamA.length == 6) {
-        break;
+      if (teamA.length >= totalNum/2) {
+        for(var i = 0; i < thisWeekPlayer.length; i++) {
+          Bscore += thisWeekPlayer[i][1];
+        }
+      break;
       }
     }
   }
